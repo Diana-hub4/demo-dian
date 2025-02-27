04d7b24411aae2462a376dceb576e98ab2e76f5f
JenifferVega <kotomivega@gmail.com>
2025-02-27 11:47:01 -0500

    actualizacion sistema contabilidad

---
+++ src/app/pages/support-document/support-document.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-document',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-document.component.html',
+  styleUrl: './support-document.component.css'
+})
+export class SupportDocumentComponent {
+
+}
